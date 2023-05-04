4d996703eaadfb382b4a3d2afa03b807bf1306ef
Rifqi Ramdhani <rifqiramdhani71@yahoo.co.id>
2023-05-04 20:15:30 +0700

    deploy

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,12 @@
 import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas';
-import Hero from './Hero';
-import Navbar from './Navbar';
-import About from './About';
-import Tech from './Tech';
-import Experience from './Experience';
-import Works from './Works';
-import Feedbacks from './Feedbacks';
-import Contact from './Contact';
+import Hero from './Hero.jsx';
+import Navbar from './Navbar.jsx';
+import About from './About.jsx';
+import Tech from './Tech.jsx';
+import Experience from './Experience.jsx';
+import Works from './Works.jsx';
+import Feedbacks from './Feedbacks.jsx';
+import Contact from './Contact.jsx';
 
 export {
   Hero,
